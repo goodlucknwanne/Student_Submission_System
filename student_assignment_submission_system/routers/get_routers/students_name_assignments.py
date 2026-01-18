79a7920b48a8ca65d0c64ffd8e494f979cf35611
Goodluck Nwanne <140513359+goodlucknwanne@users.noreply.github.com>
2026-01-18 23:18:15 +0100

    Update students_name_assignments.py

--- student_assignment_submission_system/routers/get_routers/students_name_assignments.py
+++ student_assignment_submission_system/routers/get_routers/students_name_assignments.py
@@ -2,10 +2,10 @@
 from fastapi import APIRouter, HTTPException
 from schemas.db import assignments_by_id, students_by_name
 
-assignment_router = APIRouter()
+assignment_student_router = APIRouter()
 
 
-@assignment_router.get("/students/{name}/assignments/")
+@assignment_student_router.get("/students/{name}/assignments/")
 def get_student_assignment(name: str):
     if name not in students_by_name:
         raise HTTPException(status_code=404, detail="Student not found")
