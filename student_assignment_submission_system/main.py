ec50376932dcf17d42253893fa7ed5109a1b9ec6
Goodluck Nwanne <140513359+goodlucknwanne@users.noreply.github.com>
2026-01-18 23:23:00 +0100

    Add files via upload

---
+++ student_assignment_submission_system/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+# Importing Post routers
+from routers.post_routers.teacher import teacher_router
+from routers.post_routers.student import student_router
+from routers.post_routers.assignment import assignment_router
+from routers.post_routers.assignments_id_comment import assingment_comment_router
+# Importing Get routers
+from routers.get_routers.student_name_assignments import assignment_student_router
+from routers.get_routers.assignments import router
+
+
+app = FastAPI()
+
+
+@app.include_router(teacher_router, prefix="/teacher", tags=["Teacher Register"])
+@app.include_router(student_router, prefix="/student", tags=["Student Register"])
+@app.include_router(assignment_router, prefix="/assignment", tags=["Assignment Create"])
+@app.include_router(assingment_comment_router, prefix="/assignment/comment", tags=["Comment on Assignments"])
+@app.include_router(assingment_student_router, tags=["Get Specific Student Assignments"])
+@app.include_router(assingment_comment_router, tags=["Comment on Student Assignment"])
+@app.include_router(router, prefix="/assignments", tags=["Get All Submitted Assignments"])
