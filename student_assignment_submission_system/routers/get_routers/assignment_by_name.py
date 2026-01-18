b0388f75a608782bef02c4dcd0aa59d59c8ed987
Goodluck Nwanne <140513359+goodlucknwanne@users.noreply.github.com>
2026-01-18 22:31:07 +0100

    Add files via upload

---
+++ student_assignment_submission_system/routers/get_routers/assignment_by_name.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, HTTPException
+from schemas.db import assignments_by_id, students_by_name
+
+assignment_router = APIRouter()
+
+
+@assignment_router.get("/students/{name}/assignments/")
+def get_student_assignment(name: str):
+    if name not in students_by_name:
+        raise HTTPException(status_code=404, detail="Student not found")
+    
+    student_assignment = [
+        assignment for assignment in assignments_by_id.values() 
+        if assignment.student_name == name
+        ]
+    
+    return student_assignment
