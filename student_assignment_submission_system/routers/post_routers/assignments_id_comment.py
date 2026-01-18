58b64b2b4bc1cfbc373b17716ef7b4e638886418
Oyewande <oyewandeoyediran@gmail.com>
2026-01-18 22:53:18 +0100

    Updated the assignment router and added the comment router

--- student_assignment_submission_system/routers/post_routers/assignments_id_comment.py
+++ student_assignment_submission_system/routers/post_routers/assignments_id_comment.py
@@ -1,1 +1,29 @@
 # Teacher adds a comment
+from fastapi import APIRouter, HTTPException, status
+from core.db import assignments_by_id
+from schemas.comments import CommentCreate
+from models.assignment import Assignment
+
+assignment_router = APIRouter()
+
+@assignment_router.post(
+    "/assignments/{assignment_id}/comments",
+    response_model=Assignment,
+    status_code=status.HTTP_201_CREATED
+)
+def add_comment(
+    assignment_id: int,
+    comment_data: CommentCreate
+):
+
+    assignment = assignments_by_id.get(assignment_id)
+
+    if not assignment:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Assignment not found"
+        )
+
+    assignment.comments.append(comment_data.comment)
+
+    return assignment
