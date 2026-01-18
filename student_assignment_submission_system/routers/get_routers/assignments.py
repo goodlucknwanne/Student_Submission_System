706262eee3639d7b9fc75e6392d9e3e9a9ce1972
GreatGrant <greatgrant116@gmail.com>
2026-01-18 20:41:02 +0100

    Add GET endpoint to retrieve all submitted assignments

--- student_assignment_submission_system/routers/get_routers/assignments.py
+++ student_assignment_submission_system/routers/get_routers/assignments.py
@@ -1,1 +1,10 @@
-# List all submitted assignments
+from fastapi import APIRouter
+from typing import List
+from core.db import assignments_by_id
+
+router = APIRouter()
+
+
+@router.get("/assignments")
+async def get_assignments():
+        return list(assignments_by_id.values())
