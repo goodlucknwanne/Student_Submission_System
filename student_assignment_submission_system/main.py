a77af498f31094a78221f2bef2aad5887b695572
Goodluck Nwanne <140513359+goodlucknwanne@users.noreply.github.com>
2026-01-18 23:58:32 +0100

    Fix import and router usage in main.py

--- student_assignment_submission_system/main.py
+++ student_assignment_submission_system/main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 # Importing Post routers
 from routers.post_routers.teacher import teacher_router
 from routers.post_routers.student import student_router
-from routers.post_routers.assignment import assignment_router
+from routers.post_routers.assignments import assignment_router
 from routers.post_routers.assignments_id_comment import assingment_comment_router
 # Importing Get routers
 from routers.get_routers.student_name_assignments import assignment_student_router
-from routers.get_routers.assignments import router
+from routers.get_routers.assignments import get_assignment_router
 
 
 app = FastAPI()
@@ -17,5 +17,4 @@ app = FastAPI()
 @app.include_router(assignment_router, prefix="/assignment", tags=["Assignment Create"])
 @app.include_router(assingment_comment_router, prefix="/assignment/comment", tags=["Comment on Assignments"])
 @app.include_router(assingment_student_router, tags=["Get Specific Student Assignments"])
-@app.include_router(assingment_comment_router, tags=["Comment on Student Assignment"])
-@app.include_router(router, prefix="/assignments", tags=["Get All Submitted Assignments"])
+@app.include_router(get_assignment_router, prefix="/assignments", tags=["Get All Submitted Assignments"])
