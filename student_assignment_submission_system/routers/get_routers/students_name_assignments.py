3e4797416edb4727c7135f6c927a3f4d383c520c
Goodluck Nwanne <140513359+goodlucknwanne@users.noreply.github.com>
2026-01-18 23:04:25 +0100

    Update students_name_assignments.py

--- student_assignment_submission_system/routers/get_routers/students_name_assignments.py
+++ student_assignment_submission_system/routers/get_routers/students_name_assignments.py
@@ -1,1 +1,18 @@
 # View assignments from a specific student
+from fastapi import APIRouter, HTTPException
+from schemas.db import assignments_by_id, students_by_name
+
+assignment_router = APIRouter()
+
+
+@assignment_router.get("/students/{name}/assignments/")
+def get_student_assignment(name: str):
+    if name not in students_by_name:
+        raise HTTPException(status_code=404, detail="Student not found")
+    
+    student_assignment = [
+        assignment for assignment in assignments_by_id.values() 
+        if assignment.student_name == name
+        ]
+    
+    return student_assignment
